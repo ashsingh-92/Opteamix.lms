df1caf999039dac7f4ca9b5273a935b672f5c151
ashsingh-92 <aishwaryasingh.2092@gmail.com>
2025-09-09 11:11:22 +0530

    Update supabase-wrapper.js

--- supabase-wrapper.js
+++ supabase-wrapper.js
@@ -1,3 +1,1 @@
-// supabase-wrapper.js
-// Re-export createClient from Supabase ESM CDN so index.html can import it locally
 export { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.57.2/+esm";
